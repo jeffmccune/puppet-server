49dad0ee99ce1cda845b31fa95f4baafc1331fb3
Jeff McCune <jeff@puppetlabs.com>
2015-06-01 16:08:21 -0700

    (SERVER-297) Add jruby subcommand integration tests
    Without this patch there are not tests covering the behavior of the irb,
    ruby, and gem JRuby subcommands.  This patch addresses the problem by
    adding integration tests that assert against standard output and the
    exit status.
    
    NB this patch differs from the patch against master in that the
    ruby-load-path setting is under the os-settings configuration section in
    this patch while it is under the jruby-puppet section in the patch
    against master for puppetserver 2.0

--- src/clj/puppetlabs/services/jruby/jruby_puppet_schemas.clj
+++ src/clj/puppetlabs/services/jruby/jruby_puppet_schemas.clj
@@ -4,7 +4,7 @@
   (:import (java.util.concurrent BlockingDeque)
            (clojure.lang Atom Agent IFn)
            (com.puppetlabs.puppetserver PuppetProfiler JRubyPuppet EnvironmentRegistry)
-           (org.jruby Main)
+           (org.jruby Main Main$Status)
            (org.jruby.embed ScriptingContainer)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -146,6 +146,10 @@
   [x]
   (instance? Main x))
 
+(defn jruby-main-status-instance?
+  [x]
+  (instance? Main$Status x))
+
 (defn poison-pill?
   [x]
   (instance? PoisonPill x))
@@ -167,3 +171,6 @@
 
 (def JRubyMain
   (schema/pred jruby-main-instance?))
+
+(def JRubyMainStatus
+  (schema/pred jruby-main-status-instance?))
